48eda5a0d6d36d049901c3025dc872134c6332b4
DarkBlue2021 <weihai.he@hotmail.com>
2021-11-09 06:33:36 +1100

    Update Score.sol

--- Score.sol
+++ Score.sol
@@ -1,4 +1,5 @@
 
+
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.0;
@@ -7,8 +8,18 @@ contract Score{
     
 uint score;
 address owner;
+uint my_sore = score_list[owner];
 
 event NewScore(uint _newScore);
+
+event Score_set(uint);
+
+mapping(address => uint) score_list;
+
+struct Funder{
+    address addr;
+    uint amount;
+}
 
 constructor(){
     owner = msg.sender;
@@ -16,6 +27,12 @@ constructor(){
 
 modifier onlyOwner{
     if (msg.sender == owner){
+        _;
+    }
+}
+
+modifier Fee (uint fee){
+    if (msg.value == fee){
         _;
     }
 }
@@ -28,6 +45,11 @@ function setScore(uint _newScore) public onlyOwner{
     score = _newScore;
     
     emit NewScore(score);
+    emit Score_set(_newScore);
+}
+
+function getUserScore(address user) public view returns (uint){
+    return score_list[user];
 }
 
 }
